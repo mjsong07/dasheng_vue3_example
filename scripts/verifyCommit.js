f99a3d519db1a6b70e84ddde14d887baf1638a9e
shengxinjing <316783812@qq.com>
2021-11-28 20:56:09 +0800

    ci: add commit verify

---
+++ scripts/verifyCommit.js
@@ -0,0 +1,20 @@
+
+const msg = require('fs')
+  .readFileSync('.git/COMMIT_EDITMSG', 'utf-8')
+  .trim()
+  
+
+const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.log('commit verify not ok')
+
+  console.error(`git commit的信息格式不对, 需要使用 title(scope): desc的格式
+    比如 fix: xxbug
+    feat(test): add new 
+    具体校验逻辑看 scripts/verifyCommit.js
+  `)
+  process.exit(1)
+}else{
+  console.log('commit verify very ok')
+}
